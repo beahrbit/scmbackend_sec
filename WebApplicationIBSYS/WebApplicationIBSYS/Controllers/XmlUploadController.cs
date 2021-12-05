0962c3af5b3de225e54e8509e11e88dfbff54f66
beahrbit <34711822+beahrbit@users.noreply.github.com>
2021-12-05 08:07:02 +0100

    Init

---
+++ WebApplicationIBSYS/WebApplicationIBSYS/Controllers/XmlUploadController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationIBSYS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class XmlUploadController : ControllerBase
+    {
+        public IWebHostEnvironment _environment;
+        public XmlUploadController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public class XmlUploadAPI
+        {
+            public IFormFile files { get; set; }
+        }
+
+        [HttpPost]
+        public async Task<string> Post (XmlUploadAPI objFile)
+        {
+            try
+            {
+                if (objFile.files.Length > 0)
+                {
+                    if (!Directory.Exists(_environment.WebRootPath + "\\Upload\\"))
+                    {
+                        Directory.CreateDirectory(_environment.WebRootPath + "\\Upload\\");
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\Upload\\" + objFile.files.FileName))
+                    {
+                        objFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "\\Upload\\" + objFile.files.FileName;
+                    }
+                }
+                else
+                {
+                    return "Failed";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message.ToString();
+            }
+        }
+    }
+}
